eeedadada35667f898caf931de9fd8e7e3bad188
Rickroll <amazingrickroll@gmail.com>
2024-11-05 17:18:44 +0000

    Send Correlation ID

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
+
+fn process(mut stream: TcpStream) {
+    let message_size = 0 as u32;
+    stream.write(&message_size.to_be_bytes()).unwrap();
+
+    let correlation_id = 7 as u32;
+    stream.write(&correlation_id.to_be_bytes()).unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                process(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
